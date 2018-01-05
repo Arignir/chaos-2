8d5d30352884748e6b8a80c124238bde5c6bd1ac
Benjamin Grange <benjamin.grange@epitech.eu>
2018-01-05 14:00:06 +0100

    Added hexdump().

---
+++ include/kernel/hexdump.h
@@ -0,0 +1,19 @@
+/* ------------------------------------------------------------------------ *\
+**
+**  This file is part of the Chaos Kernel, and is made available under
+**  the terms of the GNU General Public License version 2.
+**
+**  Copyright (C) 2017 - Benjamin Grange <benjamin.grange@epitech.eu>
+**
+\* ------------------------------------------------------------------------ */
+
+#ifndef _KERNEL_HEXDUMP_H_
+# define _KERNEL_HEXDUMP_H_
+
+# include <chaosdef.h>
+
+# define HEXDUMP_WIDTH		10
+
+void	hexdump(uchar *start, size_t size);
+
+#endif /* !_KERNEL_HEXDUMP_H_ */
---
+++ kernel/hexdump.c
@@ -0,0 +1,72 @@
+/* ------------------------------------------------------------------------ *\
+**
+**  This file is part of the Chaos Kernel, and is made available under
+**  the terms of the GNU General Public License version 2.
+**
+**  Copyright (C) 2017 - Benjamin Grange <benjamin.grange@epitech.eu>
+**
+\* ------------------------------------------------------------------------ */
+
+#include <kernel/hexdump.h>
+#include <stdio.h>
+
+static void
+hexdump_char(uchar *start, size_t len)
+{
+	size_t i = 0;
+
+	while (i < len) {
+		printf("%02x ", start[i]);
+		++i;
+	}
+	while (i < HEXDUMP_WIDTH) {
+		printf("   ", start[i]);
+		++i;
+	}
+}
+
+static void
+hexdump_str(uchar *start, size_t len)
+{
+	size_t i = 0;
+	char c;
+
+	while (i < len) {
+		c = start[i];
+		if (c >= ' ' && c <= '~') { /* is_printable() */
+			printf("%c", c);
+		} else {
+			printf(".");
+		}
+		++i;
+	}
+	while (i < HEXDUMP_WIDTH) {
+		printf(" ");
+		++i;
+	}
+}
+
+/*
+** Dumps the given memory
+*/
+void
+hexdump(uchar *start, size_t size)
+{
+	uchar *end;
+	size_t len;
+
+	end = start + size;
+	while (start < end)
+	{
+		len = end - start;
+		if (len >= HEXDUMP_WIDTH) {
+			len = 10;
+		}
+		printf("%p ", start);
+		hexdump_char(start, len);
+		printf("|");
+		hexdump_str(start, len);
+		printf("|\n");
+		start += HEXDUMP_WIDTH;
+	}
+}
