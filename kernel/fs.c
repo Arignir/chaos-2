2b7ee8e107dc63bf4d47ce8f4bfd436e15dc7753
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-02-17 02:33:08 +0100

    removed file tests
    deemed usless.

--- kernel/fs.c
+++ kernel/fs.c
@@ -530,40 +530,6 @@ fs_readdir(struct dir_handle *dir_handle, struct dirent *dirent)
 }
 
 /*
-** Tests for the file system.
-** Optional, can be removed.
-*/
-static void
-fs_tests(void)
-{
-	struct file_handle *fh;
-	struct dir_handle *rootdir;
-	struct dirent dirent;
-	status_t result;
-	char buff[26];
-	char name[257];
-	size_t r;
-
-	assert_eq(fs_open("/", &fh), OK);
-	assert_eq(fs_opendir(fh, &rootdir), OK);
-	while (!(result = fs_readdir(rootdir, &dirent))) {
-		printf("[dumbfs] root contains file (25 bytes max): %.25s\n", dirent.name);
-		name[0] = '/';
-		name[1] = 0;
-		strcat(name, dirent.name);
-		r = 25;
-		assert_eq(fs_open(name, &fh), OK);
-		assert_eq(fs_read(fh, buff, &r), OK);
-		assert_eq(fs_close(fh), OK);
-		buff[r] = 0;
-		printf("[dumbfs] '%.25s' contents (first 25 bytes): %s\n", dirent.name, buff);
-	}
-	assert_eq(result, ERR_END_OF_DIRECTORY);
-	assert_eq(fs_closedir(rootdir), OK);
-	printf("[Filesystem tests done]\n");
-}
-
-/*
 ** Initialises file system
 ** Runs some tests (removable).
 */
@@ -573,7 +539,6 @@ init_fs(void)
 	/* Mount initrd on root */
 	assert_eq(fs_mount("/", "dumbfs", "initrd"), OK);
 	printf("Filesystem 'dumbfs' mounted on '/'.\n");
-	fs_tests();
 }
 
 NEW_INIT_HOOK(filesystem, &init_fs, INIT_LEVEL_FILESYSTEM);
