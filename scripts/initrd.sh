956ef5826d793bab2dd7f83eb561fa8240b74855
Gaspard-- <jakob.kellendonk@epitech.eu>
2018-02-17 02:49:17 +0100

    Fixed dumbfs generation, removed usless test files

--- scripts/initrd.sh
+++ scripts/initrd.sh
@@ -26,5 +26,38 @@ if ! which dd &> /dev/null; then
 	exit 1
 fi
 
-# For now, initial ramdisk is the dumbfs filesystem
-cat "$DUMBFS" > "$OUTPUT_PATH"
+shopt -s nullglob
+
+>${OUTPUT_PATH}
+
+add_int() {
+	local x=${1}
+	local out;
+	printf -v out '\\x%02x\\x%02x\\x%02x\\x%02x' $((x & 0xFF)) $((x >> 8 & 0xFF)) $((x >> 16 & 0xFF)) $((x >> 24 & 0xFF))
+	printf ${out} >> ${OUTPUT_PATH}
+}
+
+add_file() {
+	echo -E "[DUMBFS_GEN]	adding ${1} to dumbfsfile system"
+
+	local file_name=${1}
+	local file_path=${DUMBFS_DATA}/${file_name}
+	local name_size=${#file_name}
+	local file_size=$(stat -Lc%s ${file_path})
+	local entry_size=$((file_size + name_size + 1))
+	add_int ${entry_size}
+	add_int ${file_size}
+	echo -nE "${file_name}" >> ${OUTPUT_PATH}
+	printf '\x00' >> ${OUTPUT_PATH}
+	cat ${file_path} >> ${OUTPUT_PATH}
+}
+
+files=(${DUMBFS_DATA}/*)
+
+echo -E "[DUMBFS_GEN]	found ${#files[@]} files"
+
+add_int ${#files[@]}
+for file in ${files[@]}
+do
+	add_file ${file##*/};
+done
